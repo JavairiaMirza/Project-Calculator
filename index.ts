8b0a22568471752becfe0bf40896d356c13a93e3
Javairia Mirza <153530535+JavairiaMirza@users.noreply.github.com>
2024-02-26 14:53:00 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,54 @@
+import inquirer from "inquirer";
+
+const answers:{
+    numberOne:number, 
+    numberTwo:number,
+    operator:string,
+
+}=await inquirer.prompt([
+    {
+        type:"number",
+        name:"numberOne",
+        message: "kindly enter your first number:"
+
+    },{
+        type:"number",
+        name:"numberTwo",
+        message: "kindly enter your second number:"
+        
+    }, {
+        type:"list",
+        name:"operator",
+        choices:["addition(+)", "subtraction(-)", "multiplicatiob(*)", "division(/)" ], 
+        message: "Select your operator:"
+    }
+
+]) 
+
+
+const {numberOne, numberTwo, operator}=answers
+
+if(numberOne && numberTwo && operator){
+
+} 
+
+
+let result:number=0;
+
+if(operator === "addition(+)"){
+    result=numberOne+numberTwo
+}else if(operator === "subtraction(-)"){
+    result=numberOne-numberTwo
+}if(operator === "multiplication(*)"){
+    result=numberOne * numberTwo}
+if (operator === "division(/)"){ 
+        result=numberOne/numberTwo
+
+        console.log("Your result is:",result)
+
+    } else {
+        console.log("Kindly enter valid input");
+    }
+
+
+
